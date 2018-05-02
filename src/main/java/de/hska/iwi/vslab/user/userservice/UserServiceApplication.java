5becb93d6a9a546ec4f8a62579016e62b70f1542
Raphael Hippe <info@raphaelhippe.de>
2018-05-02 10:31:55 +0200

    init commit

---
+++ src/main/java/de/hska/iwi/vslab/user/userservice/UserServiceApplication.java
@@ -0,0 +1,12 @@
+package de.hska.iwi.vslab.user.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+}
