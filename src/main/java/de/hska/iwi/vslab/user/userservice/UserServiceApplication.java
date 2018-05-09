48f73680c08912e8b7a45db01c5577f0a7c5d93d
Raphael Hippe <info@raphaelhippe.de>
2018-05-09 10:25:57 +0200

    user service crud done

--- src/main/java/de/hska/iwi/vslab/user/userservice/UserServiceApplication.java
+++ src/main/java/de/hska/iwi/vslab/user/userservice/UserServiceApplication.java
@@ -1,5 +1,8 @@
 package de.hska.iwi.vslab.user.userservice;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +10,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserServiceApplication {
+
+	@Autowired
+	private UserRepo userRepository;
+
+	@PostConstruct
+	public void generateTestData() {
+		userRepository.save(new User(1000L,"Alice", "password"));
+		userRepository.save(new User(1001L,"Bob", "password"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
